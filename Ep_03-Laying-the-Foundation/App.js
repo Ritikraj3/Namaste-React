b7ec053110ef2074790f78d02926b8d534dfc5b3
Ritikraj3 <ritikraj5776@gmail.com>
2025-06-06 22:58:38 +0530

    Foundation of react

--- Ep_03-Laying-the-Foundation/App.js
+++ Ep_03-Laying-the-Foundation/App.js
@@ -1,18 +1,41 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "children" }, [
-      React.createElement("h1", {}, "i am an h1 tag"),
-      React.createElement("h2", {}, "i am an h2 tag"),
-    ]),
-    React.createElement("div", { id: "children2" }, [
-      React.createElement("h1", {}, "i am an h1 tag"),
-      React.createElement("h2", {}, "i am an h2 tag"),
-    ]),
-  ]);
-  
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+//React.createElement => ReactElement - JS Object => HTMLElement(render)
+
+const heading = React.createElement("h1", { id: "heading" }, "hello React");
+
+console.log(heading);
+
+//JSX (transpiled before it reaches browser)
+// babel => converts JSX to JS
+//JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
+
+const JsxHeading = () => (
+  <h1 id="heading">
+    hello React
+  </h1>
+);
+
+//* component 
+// class-based component
+
+//?function-based component
+
+const sum = 25 + 30
+const HeadingComponent = () => {
+  return (
+    <div>
+      <JsxHeading/>
+      <h1 id="heading">
+      Namaste React functional component
+    </h1>
+      <h2>{sum}</h2>
+    </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
